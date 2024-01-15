f606d29d2f1f94a336bb37d78bf12579fc7026f3
Javi Caselles <casellesjavi@gmail.com>
2024-01-15 16:06:07 +0100

    added network module

--- data/src/main/java/com/javi/data/di/NetworkModule.kt
+++ data/src/main/java/com/javi/data/di/NetworkModule.kt
@@ -1,9 +1,26 @@
 package com.javi.data.di
 
 import com.javi.data.datasource.UserDataSource
+import com.javi.data.datasource.network.UserApi
 import com.javi.data.datasource.network.UserDataSourceImpl
+import okhttp3.OkHttpClient
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val networkModule = module {
-
+    factory { provideOkHttpClient() }
+    factory { provideUserApi(get()) }
+    single { provideRetrofit(get()) }
 }
+
+fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+    return Retrofit.Builder().baseUrl("https://randomuser.me/").client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create()).build()
+}
+
+fun provideOkHttpClient(): OkHttpClient {
+    return OkHttpClient().newBuilder().build()
+}
+
+fun provideUserApi(retrofit: Retrofit): UserApi = retrofit.create(UserApi::class.java)
