a8e809b11faeb312ca7d76c0ca2b411132e94fd0
Javi Caselles <casellesjavi@gmail.com>
2024-01-16 12:57:35 +0100

    fixed maintaining textfield state when coming back from another screen

--- presentation/src/main/java/com/javi/presentation/components/SearchItem.kt
+++ presentation/src/main/java/com/javi/presentation/components/SearchItem.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -18,7 +19,7 @@ import com.javi.presentation.R
 fun SearchItem(
     onValueChange: (String) -> Unit
 ) {
-    var text by remember { mutableStateOf("") }
+    var text by rememberSaveable  { mutableStateOf("") }
     OutlinedTextField(
         value = text,
         label = {
