a889364d7b2622427db9b230c8a06cd169ebde2a
Javi Caselles <casellesjavi@gmail.com>
2024-01-16 10:50:59 +0100

    swaped enabled with readonly in contact info item

--- presentation/src/main/java/com/javi/presentation/components/ContactInfoItem.kt
+++ presentation/src/main/java/com/javi/presentation/components/ContactInfoItem.kt
@@ -2,7 +2,9 @@ package com.javi.presentation.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -10,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.javi.presentation.R
 
 @Composable
@@ -27,10 +30,10 @@ fun ContactInfoItem(
             contentDescription = "Contact Info Icon",
             modifier = Modifier.align(CenterVertically)
         )
-
+        Spacer(modifier = Modifier.size(16.dp))
         OutlinedTextField(
             value = valueText,
-            enabled = isInEditMode,
+            readOnly = !isInEditMode,
             label = {
                 Text(text = hintText)
             },
